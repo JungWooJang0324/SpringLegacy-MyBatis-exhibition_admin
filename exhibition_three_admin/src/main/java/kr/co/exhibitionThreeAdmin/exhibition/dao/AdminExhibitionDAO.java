a47b2ee40639b457997bc3b019c0a0a29d47cddf
JungWooJang0324 <chiang030204@gmail.com>
2022-05-29 12:39:19 +0900

    복구

---
+++ exhibition_three_admin/src/main/java/kr/co/exhibitionThreeAdmin/exhibition/dao/AdminExhibitionDAO.java
@@ -0,0 +1,5 @@
+package kr.co.exhibitionThreeAdmin.exhibition.dao;
+
+public class AdminExhibitionDAO {
+
+}
---
+++ exhibition_three_admin/src/main/java/kr/co/exhibitionThreeAdmin/mybatis/MyBatisFramework.java
@@ -0,0 +1,56 @@
+package kr.co.exhibitionThreeAdmin.mybatis;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ * MyBatis Framework�� �����ϰ� MyBatis Handler�� ��ȯ
+ * @author user
+ */
+public class MyBatisFramework {
+	private static MyBatisFramework mbf;
+	private static SqlSessionFactory ssf;
+	
+	private MyBatisFramework() {
+		org.apache.ibatis.logging.LogFactory.useLog4JLogging();
+	}//MyBatisFramework
+	
+	public static MyBatisFramework getInstance() {
+		if(mbf==null) {
+			mbf= new MyBatisFramework();
+		}//end if
+		return mbf;
+	}//getInstance
+	
+	private static SqlSessionFactory getSqlSessionFactory() throws IOException {
+				if(ssf== null) {
+					Reader reader = null; 
+					try {
+						//1.�������� ����
+						reader = Resources.getResourceAsReader("kr/co/exhibitionThreeAdmin/mybatis/SqlMapConfig.xml");
+						//2. MyBatis Framework ����
+						ssf = new SqlSessionFactoryBuilder().build(reader);
+					}finally {
+						if(reader != null) {reader.close();}//end if
+					}//end finally
+				}//end if
+		
+		return ssf;
+	}//getSqlSessionFactory
+	
+	public SqlSession getMyBatisHandler() {
+		SqlSession ss = null;
+		try {
+			 ss = getSqlSessionFactory().openSession();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}//end catch
+		return ss;
+	}//getMyBatisHandler
+	
+}//class
